eac698b20a2aa8fd47a17ed9b1fa73be10df38d4
sourav3365 <82954143+sourav3365@users.noreply.github.com>
2022-01-13 09:28:17 +0530

    second commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app deployment completed"
+    return "Flask app deployment completed testing 1"
 
 port = int(os.getenv("PORT", 5001))
 
